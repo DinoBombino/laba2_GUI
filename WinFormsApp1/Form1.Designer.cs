57c0e1107a53609155d1cdf12e5d290dfbc3dc37
Brodyaga <korepanov.dmitriy.04@mail.ru>
2024-03-24 17:38:06 +0800

    Прикрутил логику

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(559, 15);
+            label1.TabIndex = 0;
+            label1.Text = "6. Дано трехзначное число. Не прибегая к использованию строк вывести наибольшую из его цифр";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(235, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 35);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Введите трехзначное число:";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDark;
+            button1.Location = new Point(399, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "ТЫК";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(586, 70);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Задача на if";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Button button1;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,74 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Properties.Settings.Default.number.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(this.textBox1.Text);
+            //MessageBox.Show("Ура работает! (╯°□°）╯︵ ┻━┻");
+
+            int number;
+            try
+            {
+                number = int.Parse(textBox1.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Некорректный ввод", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); //можно и убрать
+                textBox1.Clear();
+                return;
+            }
+
+            Properties.Settings.Default.number = number;
+            Properties.Settings.Default.Save();
+
+            //number = int.Parse(this.textBox1.Text);
+            if (!(number > 99 && number < 1000))
+            {
+                MessageBox.Show("Введено некорректное число. Введите трехзначное число.", "Ошибка");
+            }
+            else 
+            {
+                MessageBox.Show("Наибольшая цифра в числе: " + Logic.FindMaxDigit(number).ToString(), "Ответ");
+            }
+            textBox1.Clear();
+        }
+    }
+    public class Logic
+    {
+        public static int FindMaxDigit(int number)
+        {
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+
+            int maxDigit = digit1;
+            if (digit2 > maxDigit)
+            {
+                maxDigit = digit2;
+            }
+            if (digit3 > maxDigit)
+            {
+                maxDigit = digit3;
+            }
+
+            return maxDigit;
+        }
+    }
+}
